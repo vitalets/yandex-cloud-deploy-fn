8a5e58e730328c0eb84c88919aba808bdb77e388
Vitaliy Potapov <noginsk@rambler.ru>
2021-10-29 13:35:29 +0300

    logging

--- src/helpers/auth-info.ts
+++ src/helpers/auth-info.ts
@@ -3,7 +3,7 @@ import { Session } from 'yandex-cloud-lite';
 export async function getAuthInfo(session: Session) {
   const { authKeyFile, oauthToken, useCliConfig } = session.options;
   if (oauthToken) return `oauthToken`;
-  if (useCliConfig) return `cliConfig`;
+  if (useCliConfig) return `yc cli`;
   if (authKeyFile) {
     const sa = await session.getServiceAccount();
     return `authKeyFile (${sa?.name})`;
